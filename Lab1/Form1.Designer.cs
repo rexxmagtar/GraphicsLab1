a00c7b959772d8883f7f5dc523a778660f0a3f18
rexx.magtar <rexx.magtar@gmail.com>
2021-02-14 20:51:37 +0300

    lab done

---
+++ Lab1/Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ColorPicker = new System.Windows.Forms.ColorDialog();
+            this.ColorPickerButton = new System.Windows.Forms.Button();
+            this.C_Input = new System.Windows.Forms.NumericUpDown();
+            this.M_Input = new System.Windows.Forms.NumericUpDown();
+            this.Y_Input = new System.Windows.Forms.NumericUpDown();
+            this.K_Input = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Z_Input = new System.Windows.Forms.NumericUpDown();
+            this.YY_input = new System.Windows.Forms.NumericUpDown();
+            this.X_input = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.L_Input = new System.Windows.Forms.NumericUpDown();
+            this.S_Input = new System.Windows.Forms.NumericUpDown();
+            this.H_Input = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.C_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Y_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.K_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Z_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YY_input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.X_input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.L_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.S_Input)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.H_Input)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ColorPickerButton
+            // 
+            this.ColorPickerButton.Location = new System.Drawing.Point(45, 50);
+            this.ColorPickerButton.Name = "ColorPickerButton";
+            this.ColorPickerButton.Size = new System.Drawing.Size(130, 23);
+            this.ColorPickerButton.TabIndex = 0;
+            this.ColorPickerButton.Text = "Pick color";
+            this.ColorPickerButton.UseVisualStyleBackColor = true;
+            this.ColorPickerButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // C_Input
+            // 
+            this.C_Input.Location = new System.Drawing.Point(264, 171);
+            this.C_Input.Name = "C_Input";
+            this.C_Input.Size = new System.Drawing.Size(120, 22);
+            this.C_Input.TabIndex = 1;
+            this.C_Input.ValueChanged += new System.EventHandler(this.CMYK_Input_ValueChanged);
+            // 
+            // M_Input
+            // 
+            this.M_Input.Location = new System.Drawing.Point(264, 239);
+            this.M_Input.Name = "M_Input";
+            this.M_Input.Size = new System.Drawing.Size(120, 22);
+            this.M_Input.TabIndex = 2;
+            this.M_Input.ValueChanged += new System.EventHandler(this.CMYK_Input_ValueChanged);
+            // 
+            // Y_Input
+            // 
+            this.Y_Input.Location = new System.Drawing.Point(264, 306);
+            this.Y_Input.Name = "Y_Input";
+            this.Y_Input.Size = new System.Drawing.Size(120, 22);
+            this.Y_Input.TabIndex = 3;
+            this.Y_Input.ValueChanged += new System.EventHandler(this.CMYK_Input_ValueChanged);
+            // 
+            // K_Input
+            // 
+            this.K_Input.Location = new System.Drawing.Point(264, 352);
+            this.K_Input.Name = "K_Input";
+            this.K_Input.Size = new System.Drawing.Size(120, 22);
+            this.K_Input.TabIndex = 4;
+            this.K_Input.ValueChanged += new System.EventHandler(this.CMYK_Input_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 175);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "C";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "M";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(222, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(222, 357);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "K";
+            // 
+            // Z_Input
+            // 
+            this.Z_Input.DecimalPlaces = 1;
+            this.Z_Input.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.Z_Input.Location = new System.Drawing.Point(570, 306);
+            this.Z_Input.Maximum = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.Z_Input.Name = "Z_Input";
+            this.Z_Input.Size = new System.Drawing.Size(120, 22);
+            this.Z_Input.TabIndex = 11;
+            this.Z_Input.ValueChanged += new System.EventHandler(this.XYZ_Input_ValueChanged);
+            // 
+            // YY_input
+            // 
+            this.YY_input.DecimalPlaces = 1;
+            this.YY_input.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.YY_input.Location = new System.Drawing.Point(570, 238);
+            this.YY_input.Maximum = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.YY_input.Name = "YY_input";
+            this.YY_input.Size = new System.Drawing.Size(120, 22);
+            this.YY_input.TabIndex = 10;
+            this.YY_input.ValueChanged += new System.EventHandler(this.XYZ_Input_ValueChanged);
+            // 
+            // X_input
+            // 
+            this.X_input.DecimalPlaces = 1;
+            this.X_input.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.X_input.Location = new System.Drawing.Point(570, 170);
+            this.X_input.Maximum = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            this.X_input.Name = "X_input";
+            this.X_input.Size = new System.Drawing.Size(120, 22);
+            this.X_input.TabIndex = 9;
+            this.X_input.ValueChanged += new System.EventHandler(this.XYZ_Input_ValueChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(532, 310);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Z";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(532, 238);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(532, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 17);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "X";
+            // 
+            // L_Input
+            // 
+            this.L_Input.Location = new System.Drawing.Point(785, 305);
+            this.L_Input.Name = "L_Input";
+            this.L_Input.Size = new System.Drawing.Size(120, 22);
+            this.L_Input.TabIndex = 17;
+            this.L_Input.ValueChanged += new System.EventHandler(this.HSL_Input_ValueChanged);
+            // 
+            // S_Input
+            // 
+            this.S_Input.Location = new System.Drawing.Point(785, 238);
+            this.S_Input.Name = "S_Input";
+            this.S_Input.Size = new System.Drawing.Size(120, 22);
+            this.S_Input.TabIndex = 16;
+            this.S_Input.ValueChanged += new System.EventHandler(this.HSL_Input_ValueChanged);
+            // 
+            // H_Input
+            // 
+            this.H_Input.Location = new System.Drawing.Point(785, 170);
+            this.H_Input.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.H_Input.Name = "H_Input";
+            this.H_Input.Size = new System.Drawing.Size(120, 22);
+            this.H_Input.TabIndex = 15;
+            this.H_Input.ValueChanged += new System.EventHandler(this.HSL_Input_ValueChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(744, 310);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(16, 17);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "L";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(744, 242);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(17, 17);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "S";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(744, 176);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 17);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "H";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(972, 582);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.L_Input);
+            this.Controls.Add(this.S_Input);
+            this.Controls.Add(this.H_Input);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Z_Input);
+            this.Controls.Add(this.YY_input);
+            this.Controls.Add(this.X_input);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.K_Input);
+            this.Controls.Add(this.Y_Input);
+            this.Controls.Add(this.M_Input);
+            this.Controls.Add(this.C_Input);
+            this.Controls.Add(this.ColorPickerButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.C_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.M_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Y_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.K_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Z_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YY_input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.X_input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.L_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.S_Input)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.H_Input)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog ColorPicker;
+        private System.Windows.Forms.Button ColorPickerButton;
+        private System.Windows.Forms.NumericUpDown C_Input;
+        private System.Windows.Forms.NumericUpDown M_Input;
+        private System.Windows.Forms.NumericUpDown Y_Input;
+        private System.Windows.Forms.NumericUpDown K_Input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown Z_Input;
+        private System.Windows.Forms.NumericUpDown YY_input;
+        private System.Windows.Forms.NumericUpDown X_input;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown L_Input;
+        private System.Windows.Forms.NumericUpDown S_Input;
+        private System.Windows.Forms.NumericUpDown H_Input;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Lab1/Form1.cs
@@ -0,0 +1,388 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public static bool NeedToNotifyChanges = true;
+
+        public struct CMYK
+        {
+            public double c, m, y, k;
+
+            public CMYK(double c, double m, double y, double k)
+            {
+                this.c = c;
+                this.m = m;
+                this.y = y;
+                this.k = k;
+            }
+        }
+
+        public struct XYZ
+        {
+            public double x, y, z;
+
+            public XYZ(double x, double y, double z)
+            {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+        }
+
+        public struct HSL
+        {
+            public int h, s, l;
+
+            public HSL(int h, int s, int l)
+            {
+                this.h = h;
+                this.s = s;
+                this.l = l;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ColorPicker.ShowDialog() == DialogResult.Cancel)
+                return;
+            // установка цвета формы
+            this.BackColor = ColorPicker.Color;
+
+            NeedToNotifyChanges = false;
+
+            CMYK newCmyk = From_RGB_TO_CMYK(ColorPicker.Color.R, ColorPicker.Color.G, ColorPicker.Color.B);
+
+            SetCMYKParametrs(newCmyk);
+
+            XYZ newXyz = From_RGB_TO_XYZ(ColorPicker.Color.R, ColorPicker.Color.G, ColorPicker.Color.B);
+
+            SetXYZParametrs(newXyz);
+
+            HSL newHsl = RGB_TO_HSL(ColorPicker.Color.R, ColorPicker.Color.G, ColorPicker.Color.B);
+
+            SetHSLParametrs(newHsl);
+
+            NeedToNotifyChanges = true;
+        }
+
+        private void SetCMYKParametrs(CMYK color)
+        {
+            C_Input.Value = Math.Max(0, Math.Min(C_Input.Maximum, (decimal)color.c * 100));
+            M_Input.Value = Math.Max(0, Math.Min(M_Input.Maximum, (decimal)color.m * 100));
+            Y_Input.Value = Math.Max(0, Math.Min(Y_Input.Maximum, (decimal)color.y * 100));
+            K_Input.Value = Math.Max(0, Math.Min(K_Input.Maximum, (decimal)color.k * 100));
+        }
+
+        private void SetXYZParametrs(XYZ color)
+        {
+            X_input.Value = Math.Max(0, Math.Min(X_input.Maximum, (decimal)color.x));
+            YY_input.Value = Math.Max(0, Math.Min(YY_input.Maximum, (decimal)color.y));
+            Z_Input.Value = Math.Max(0, Math.Min(Z_Input.Maximum, (decimal)color.z));
+        }
+
+        private void SetHSLParametrs(HSL color)
+        {
+            H_Input.Value = Math.Max(0, Math.Min(H_Input.Maximum, (decimal)color.h));
+            S_Input.Value = Math.Max(0, Math.Min(S_Input.Maximum, (decimal)color.s));
+            L_Input.Value = Math.Max(0, Math.Min(L_Input.Maximum, (decimal)color.l));
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CMYK_Input_ValueChanged(object sender, EventArgs e)
+        {
+            if (NeedToNotifyChanges)
+            {
+                NeedToNotifyChanges = false;
+
+                var color = From_CMYK((1.0f * (int)C_Input.Value) / 100,
+                    (1.0f * (int)M_Input.Value) / 100,
+                    (1.0f * (int)Y_Input.Value) / 100,
+                    (1.0f * (int)K_Input.Value) / 100);
+
+                this.BackColor = color;
+
+                SetXYZParametrs(From_RGB_TO_XYZ(color.R, color.G, color.B));
+                SetHSLParametrs(RGB_TO_HSL(color.R, color.G, color.B));
+
+                NeedToNotifyChanges = true;
+            }
+            else
+            {
+                return;
+            }
+
+        }
+
+        private void XYZ_Input_ValueChanged(object sender, EventArgs e)
+        {
+            if (NeedToNotifyChanges)
+            {
+                NeedToNotifyChanges = false;
+
+                var color = From_XYZ_TO_RGB(new XYZ((1.0f * (int)X_input.Value),
+                    (1.0f * (int)YY_input.Value),
+                    (1.0f * (int)Z_Input.Value)));
+
+                this.BackColor = color;
+
+                SetCMYKParametrs(From_RGB_TO_CMYK(color.R, color.G, color.B));
+                SetHSLParametrs(RGB_TO_HSL(color.R, color.G, color.B));
+
+                NeedToNotifyChanges = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void HSL_Input_ValueChanged(object sender, EventArgs e)
+        {
+            if (NeedToNotifyChanges)
+            {
+                NeedToNotifyChanges = false;
+
+                var color = FROM_HSL_TO_RGB(new HSL(((int)H_Input.Value),
+                    ((int)S_Input.Value),
+                    ((int)L_Input.Value)));
+
+                this.BackColor = color;
+
+                SetCMYKParametrs(From_RGB_TO_CMYK(color.R, color.G, color.B));
+                SetXYZParametrs(From_RGB_TO_XYZ(color.R, color.G, color.B));
+
+                NeedToNotifyChanges = true;
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private Color From_CMYK(double c, double m, double y, double k)
+        {
+            int r = Math.Min(255, Math.Max(0, (int)(255 * (1 - c) * (1 - k))));
+            int g = Math.Min(255, Math.Max(0, (int)(255 * (1 - m) * (1 - k))));
+            int b = Math.Min(255, Math.Max(0, (int)(255 * (1 - y) * (1 - k))));
+
+
+            return Color.FromArgb(r, g, b);
+        }
+
+        private CMYK From_RGB_TO_CMYK(int r, int g, int b)
+        {
+            double k = Math.Min(1 - 1.0f * r / 255, Math.Min(1 - 1.0f * g / 255, 1 - 1.0f * b / 255));
+
+            if (k == 1)
+            {
+                return new CMYK(0, 0, 0, 1);
+            }
+
+            double c = Math.Max(0, (1 - (1.0f * r / 255) - k) / (1 - k));
+
+            double m = Math.Max(0, (1 - (1.0f * g / 255) - k) / (1 - k));
+
+            double y = Math.Max(0, (1 - (1.0f * b / 255) - k) / (1 - k));
+
+            return new CMYK(c, m, y, k);
+        }
+
+        private XYZ From_RGB_TO_XYZ(int r, int g, int b)
+        {
+            double Func(double x)
+            {
+                if (x >= 0.04045)
+                {
+                    return (double)Math.Pow((x + 0.055) / 1.055, 2.4);
+                }
+
+                return x / 12.92f;
+            }
+
+            double Rn = Func(1.0f * r / 255) * 100;
+
+            double Gn = Func(1.0f * g / 255) * 100;
+
+            double Bn = Func(1.0f * b / 255) * 100;
+
+            double X = 0.412453f * Rn + 0.357580f * Gn + 0.180423f * Bn;
+
+            double Y = 0.212671f * Rn + 0.715160f * Gn + 0.072169f * Bn;
+
+            double Z = 0.019334f * Rn + 0.119193f * Gn + 0.950227f * Bn;
+
+            return new XYZ(X, Y, Z);
+        }
+
+        private Color From_XYZ_TO_RGB(XYZ xyz)
+        {
+            //xyz.x /= 100;
+            //xyz.y /= 100;
+            //xyz.z /= 100;
+
+            double Func(double x)
+            {
+                if (x >= 0.0031308)
+                {
+                    return Math.Pow(x, 1 / 2.4) * 1.055f - 0.055f;
+                }
+
+                return x * 12.92f;
+            }
+
+            double Rn = 3.2406f * xyz.x / 100.0f + -1.5372f * xyz.y / 100.0f + -0.4986f * xyz.z / 100.0f;
+
+            double Gn = -0.9689f * xyz.x / 100.0f + 1.8758f * xyz.y / 100.0f + 0.0415f * xyz.z / 100.0f;
+
+            double Bn = 0.0557f * xyz.x / 100.0f + -0.2040f * xyz.y / 100.0f + 1.0570f * xyz.z / 100.0f;
+
+
+            int R = (int)Math.Max(0, (Func(Rn) * 255));
+
+            int G = (int)Math.Max(0, (Func(Gn) * 255));
+
+            int B = (int)Math.Max(0, (Func(Bn) * 255));
+
+            R = Math.Min(255, R);
+
+            G = Math.Min(255, G);
+
+            B = Math.Min(255, B);
+
+            try
+            {
+                return Color.FromArgb(R, G, B);
+            }
+            catch
+            {
+                return Color.White;
+            }
+
+        }
+
+        private HSL RGB_TO_HSL(int r, int g, int b)
+        {
+            double rS = 1.0 * r / 255;
+            double gS = 1.0 * g / 255;
+            double bS = 1.0 * b / 255;
+
+            double cMax = Math.Max(rS, Math.Max(gS, bS));
+            double cMin = Math.Min(rS, Math.Min(gS, bS));
+
+            double delta = cMax - cMin;
+
+            int H = 0;
+
+            if (delta == 0)
+            {
+                H = 0;
+            }
+            else if (cMax == rS && gS>bS)
+            {
+                H = (int)(60 * (((gS - bS) / delta) % 6));
+            }
+            else if (cMax == rS)
+            {
+                H = (int)(60 * (((gS - bS) / delta) + 6));
+            }
+
+            else if (cMax == gS)
+            {
+                H = (int)(60 * (((bS - rS) / delta) + 2));
+            }
+
+            else
+            {
+                H = (int)(60 * (((rS - gS) / delta) + 4));
+            }
+
+            int L = (int)(100 * (cMax + cMin) / 2);
+
+            int S = (int)(100 * delta / (1 - Math.Abs(2.0 * L / 100.0 - 1)));
+
+            return new HSL(H, S, L);
+
+        }
+
+        private Color FROM_HSL_TO_RGB(HSL color)
+        {
+            double s = 1.0 * color.s / 100;
+
+            double l = 1.0 * color.l / 100;
+
+            double C = (1 - Math.Abs(2 * l - 1)) * s;
+
+            double X = C * (1 - Math.Abs((1.0 * color.h / 60) % 2 - 1));
+
+            double m = l - C / 2;
+
+            double rS = 0, gS = 0, bS = 0;
+
+            if (0 <= color.h && color.h < 60)
+            {
+                rS = C;
+                gS = X;
+                bS = 0;
+            }
+            else if (60 <= color.h && color.h < 120)
+            {
+                rS = X;
+                gS = C;
+                bS = 0;
+            }
+            else if (120 <= color.h && color.h < 180)
+            {
+                rS = 0;
+                gS = C;
+                bS = X;
+            }
+            else if (180 <= color.h && color.h < 240)
+            {
+                rS = 0;
+                gS = X;
+                bS = C;
+            }
+            else if (240 <= color.h && color.h < 300)
+            {
+                rS = X;
+                gS = 0;
+                bS = C;
+            }
+            else if (300 <= color.h && color.h <= 360)
+            {
+                rS = C;
+                gS = 0;
+                bS = X;
+            }
+
+            int R = Math.Min(255, Math.Max(0, (int)((rS + m) * 255)));
+            int G = Math.Min(255, Math.Max(0, (int)((gS + m) * 255)));
+            int B = Math.Min(255, Math.Max(0, (int)((bS + m) * 255)));
+
+            return Color.FromArgb(R, G, B);
+        }
+    }
+}
